73bd6209a29685e409dc56d18c297ae6b760f83a
Tobias Kloht <mail@kloht.com>
2017-06-22 17:38:35 +0200

    during search auto-add unknown feeds
    for each search result item (feed)
      if item is in database, add id to item
      else insert feed into database, then add id of new item to db

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -2,11 +2,35 @@ defmodule PodcastsApi.SearchController do
   use PodcastsApi.Web, :controller
   alias PodcastsApi.Feed
 
+  import PodcastsApi.ParseFeed
+  import URI
+
+  # returns id
+  def insert_feed(parsed_feed) do
+    # IO.puts "in insert feed..."
+    # IO.inspect parsed_feed
+    changeset = Feed.changeset %Feed{}, parsed_feed
+    case Repo.insert changeset do
+      {:ok, feed} ->
+        # IO.puts "inserted feed..."
+        # IO.inspect feed
+        %{:id => id} = feed
+        id
+      {:error, changeset} ->
+        IO.puts(">>>>> unable to insert changeset: " )
+        # IO.inspect(changeset )
+        nil
+    end 
+  end
+
   def get(conn, %{"term" => term} = params) do
 
     # term = "accidental"
-
-    url = "https://itunes.apple.com/search?term=#{term}&entity=podcast"
+    # 
+    encoded = encode(term)
+    IO.puts "seaerch term: " <> term
+    IO.puts "encoded: " <> encoded
+    url = "https://itunes.apple.com/search?term=#{encoded}&entity=podcast"
 
     case HTTPoison.get(url) do
 
@@ -35,16 +59,19 @@ defmodule PodcastsApi.SearchController do
                 [head | tail] ->
                   IO.puts "feed is already in library: "
                   IO.inspect head
-                  result = Map.put(feed, "feed_id", head)
-                  IO.inspect result
-                  result
+                  Map.put(feed, "feed_id", head)
                   # result = %{feed | "feed_id" => head}
                 _ ->
-                  result = feed
+                  case HTTPoison.get(x, [], [ ssl: [{:versions, [:'tlsv1.2']}] ]) do
+                    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+                      case parseFeed(x, body) do
+                        {:ok, parsed} -> Map.put(feed, "feed_id", insert_feed(parsed))
+                        _ -> feed
+                      end
+                    _ -> feed
+                   feed
+                end
               end
-              # IO.puts "is feed already in library?"
-              # IO.inspect(result)
-              result
             end)
 
           responseWithIds = Map.put(response, "results", feedWithIds)
